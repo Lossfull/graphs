4226d54aaadeea99749330017893438d01894aa4
KoljanTestwamore <anton.pab@gmail.com>
2020-04-27 23:32:47 +0300

    small updates to parser

--- parser.py
+++ parser.py
@@ -3,8 +3,11 @@ import xml.etree.ElementTree as ET
 def main():
     tree = ET.parse('planet.osm')
     root = tree.getroot()
+    x = 0
     for child in root:
-        print(child.tag)
+        if child.tag == 'way':
+            x = x + 1
+    print(x)
 
 if __name__ == "__main__":
     main()
