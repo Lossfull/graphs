5d5fc4ccc034dd5a8c039affd674858d35d804cd
KoljanTestwamore <anton.pab@gmail.com>
2020-04-27 23:20:27 +0300

    Created parser

--- parser.py
+++ parser.py
@@ -1,1 +1,10 @@
 import xml.etree.ElementTree as ET 
+
+def main():
+    tree = ET.parse('planet.osm')
+    root = tree.getroot()
+    for child in root:
+        print(child.tag)
+
+if __name__ == "__main__":
+    main()
