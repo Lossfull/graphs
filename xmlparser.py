6d94c8ff48c7a30f0b4892d42895dd7eb2150f84
KoljanTestwamore <anton.pab@gmail.com>
2020-04-28 14:31:39 +0300

    Some parser updates

--- xmlparser.py
+++ xmlparser.py
@@ -4,14 +4,18 @@ def main():
     tree = ET.parse('planet.osm')
     db = tree.getroot()
     x = 0
+    highways = []
     for child in db:
         if child.tag == 'way':
             way = child
             print("\n id =", way.attrib['id'])
-            for element in way:
-                atrs = element.attrib
-    print(':)')
-    255
+            vals = {}
+            for element in way.iter('tag'):
+                vals[element.attrib['k']] = element.attrib['v']
+                #print(, element.attrib['v'])
+            highways.append(vals)
+            print(vals)
+    highways
 
 if __name__ == "__main__":
     main()
